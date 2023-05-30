64673999cadb6e955ce3037b46479596d8fd1f53
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-05-30 19:50:48 -0400

    Finished Api Fetching

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
 import FetchApi from './api/fetch';
-import Search from './components/search/search';
 
 function App() {
+  const [videos, setVideos] = useState([]);
 
-  FetchApi()
+  useEffect(() => {
+    FetchApi("new-york")
+      .then((data) => {
+        setVideos(data.items);
+        
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+  
+console.log(videos)
 
   return (
     <div className="App">
-      <Search />
+      <h2>My Videos</h2>
+      {videos.map((videoTitle) => {
+        return <div key={videoTitle.id}>{videoTitle.snippet.title}</div>;
+      })}
     </div>
   );
 }
--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,16 +1,9 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-function FetchApi() {
-  return fetch(
-    `https://youtube.googleapis.com/youtube/v3/search?key=${API_KEY}`
-  )
+function FetchApi(searchKeyword) {
+  return fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=${searchKeyword}&key=${API_KEY}`)
     .then((response) => response.json())
-    .then((data) => {
-      // Process the fetched data
-      console.log(data);
-    })
     .catch((error) => {
-      // Handle any errors
       console.error("Error:", error);
     });
 }
