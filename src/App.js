a62f322c8f34b824db93c0025689b77f88227cf7
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-06-06 22:05:27 -0400

    adding Router to app.js file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import './App.css';
-import React, { useState } from 'react';
-import SearchBar from './components/search/SearchBar.js';
-import VideoIndex from './components/video-index/VideoIndex';
-import Navbar from './components/navbar/Navbar';
+import "./App.css";
 
+import React, { useState } from "react";
+import SearchBar from "./components/search/SearchBar.js";
+import VideoIndex from "./components/video-index/VideoIndex";
+import Navbar from "./components/navbar/Navbar";
+import About from "./components/about/About";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
   const [videos, setVideos] = useState([]);
-	const [searchTerm, setSearchTerm] = useState("")
+  const [searchTerm, setSearchTerm] = useState("");
 
-
-
-  console.log(videos)
-
+  console.log(videos);
 
   return (
-    <div className="App">
-      <Navbar />
-      <SearchBar className="searchBar" setVideos={setVideos} searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-      <VideoIndex videos={videos}/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/">
+          <SearchBar
+            className="searchBar"
+            setVideos={setVideos}
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+          />
+          <VideoIndex videos={videos} />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,24 +1,22 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-function Navbar() {
-    return (
-       <div className="nav-box">
-        <header>
-            <ul className="navbar">
-                <li className="site-title">
-                    <Link to="/">Youtube</Link>
-                </li>
-                <li className="list-item">
-                <Link to="/">Home</Link>
-                </li>
-                <li className="list-item">
-                <Link to="/About">About</Link>
-                </li>
-            </ul>
-        </header>
-       </div>
-    );
+export default function Navbar() {
+  return (
+    <div className="nav-box">
+      <header>
+        <ul className="navbar">
+          <li className="site-title">
+            <Link to="/">Youtube</Link>
+          </li>
+          <li className="list-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="list-item">
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
 }
-
-export default Navbar;
