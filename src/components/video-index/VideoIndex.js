ae4b9789c405a9854d61a767b9347fb556c60f4f
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-06-07 00:05:46 -0400

    update video index

--- src/components/video-index/VideoIndex.js
+++ src/components/video-index/VideoIndex.js
@@ -1,58 +1,32 @@
 import "./VideoIndex.css";
+import VideoShow from "./VideoShow";
 
 export default function VideoIndex({ videos }) {
+
   return (
     <div>
       <h1>Video List</h1>
-      {videos.map((video) => {
-        let videoId = video.id.videoId;
-        let thumbnail = video.snippet.thumbnails.high.url;
-        let title = video.snippet.title;
-
-        return (
-          <div className="container">
-            <div className="video-index">
-              <div className="video-item">
+      {videos.length > 0 ? (
+        videos.map((video) => {
+          let videoId = video.id.videoId;
+          let thumbnail = video.snippet.thumbnails.medium.url;
+          let title = video.snippet.title;
+          
+          return (
+            <div className="container">
+              <div className="video-index">
                 <div className="video-img">
-                  <img src={thumbnail} alt="" />
+                  <VideoShow videoId={videoId} thumbnail={thumbnail} title={title} />
+                  {/* <img src={thumbnail} alt="" /> */}
+                {/* <div className="video-item"></div>
+                <div className="video-description">{title}</div> */}
                 </div>
-                <div className="video-description">{title}</div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+
+      ) : ("some type of message")}
     </div>
   );
 }
-
-
-// import "./VideoIndex.css";
-// import YouTube from "react-youtube"
-
-// export default function VideoIndex({ videos }) {
-//   return (
-//    <div>
-//     <h1>Video List</h1>
-//      {/* { if (videos.length>0){ */}
-//           {videos.map((video) => {
-//             let videoId = "https://www.youtube.com/watch?v=" + video.id.videoId 
-//             // let thumbnail = video.snippet.thumbnails.high
-//             // let title = video.snippet.title
-//             <div className="container">
-//             <div className="video-index">
-//               <div className="video-item">
-//                 <div className="video-img">{ videoId}</div>
-//                 {/* <img src={video.snippet.thumbnails.high} alt="" /> */}
-//                 <div className="video-description">video 1</div>
-//               </div>
-//             </div>
-//           </div>
-//           })}
-//         {/* }
-//       } */}
-  
-//   </div>
-//   );
-    
-// }
