b7f5b2d434f26577fba98f5facfb0ea8eb11f373
James Edmond <jamesedmond@pursuit.org>
2023-06-06 22:00:15 -0400

    using link

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,24 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-export default function Navbar() {
+function Navbar() {
     return (
        <div className="nav-box">
         <header>
             <ul className="navbar">
-                <li className="site-title"><span>Youtube</span></li>
-                <li className="list-item">Home</li>
-                <li className="list-item">About</li>
+                <li className="site-title">
+                    <Link to="/">Youtube</Link>
+                </li>
+                <li className="list-item">
+                <Link to="/">Home</Link>
+                </li>
+                <li className="list-item">
+                <Link to="/About">About</Link>
+                </li>
             </ul>
         </header>
        </div>
     );
 }
+
+export default Navbar;
