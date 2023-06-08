d643392ff20e64486141a9ecd63f849f5af67429
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-06-08 07:33:14 -0400

    updating modal window

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
-
 import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch, useLocation } from "react-router-dom";
 import SearchBar from "./components/search/SearchBar.js";
 import VideoIndex from "./components/video-index/VideoIndex";
 import Navbar from "./components/navbar/Navbar";
 import About from "./components/about/About";
 import Modal from "./components/modal-window/Modal";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [videos, setVideos] = useState([]);
@@ -18,22 +16,20 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar className="navbar"/>
-        <Route exact path="/">
-          <SearchBar
-            className="searchBar"
-            setVideos={setVideos}
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-          />
-          <VideoIndex className="videoIndex" videos={videos} />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="*">
-          <Modal />
-        </Route>
+        <Navbar className="navbar" />
+        <Switch>
+          <Route exact path="/">
+            <SearchBar
+              className="searchBar"
+              setVideos={setVideos}
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+            />
+            <VideoIndex className="videoIndex" videos={videos} />
+          </Route>
+          <Route path="/about" component={About} />
+          <Route path="/*" component={Modal} />
+        </Switch>
       </div>
     </Router>
   );
--- src/components/modal-window/Modal.js
+++ src/components/modal-window/Modal.js
@@ -1,30 +1,27 @@
-import "./Modal.css"
-import { useState } from "react"
-export default function Modal(){
-    const [modal, setModal] = useState(false)
-function toggleModal(){
-    setModal(!modal)
-}
-return (
-    <> 
-    <div className = "overlay">
-        <button className="open-modal" onClick ={toggleModal}>OPEN-MODAL </button>
-    </div>
+import React, { useState } from "react";
+import "./Modal.css";
+import { useLocation, Route } from "react-router-dom";
 
-    {modal && (
-          <div className= "modal-window">
-          <div className= "modal-box"> 
-          <p>Modal window!
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <button className= "close-modal" onClick={toggleModal}>CLOSE</button>
+export default function Modal() {
+  const location = useLocation();
+  const [modalOpen, setModalOpen] = useState(true);
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <>
+      {location.pathname !== "/" && modalOpen && (
+        <Route path="*">
+          <div className="modal-window">
+            <div className="modal-box">
+              <p>Please check the URL.</p>
+              <button className="close-modal" onClick={closeModal}>X</button>
+            </div>
           </div>
-   
-      </div>
-    )}
-  
-    
+        </Route>
+      )}
     </>
-    
-)
-};
+  );
+}
